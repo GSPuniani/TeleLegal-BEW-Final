7d4c5e7c43e62278dbe2422d6173a4a20be6abe6
Gobind Puniani <gobindpuniani@gmail.com>
2021-03-05 18:55:18 -0800

    Updated some routes

--- telelegal_app/main/routes.py
+++ telelegal_app/main/routes.py
@@ -2,10 +2,9 @@
 from flask import Blueprint, request, render_template, redirect, url_for, flash
 from flask_login import login_user, logout_user, login_required, current_user
 from datetime import date, datetime
-from telelegal_app.models import User, Forum, Requests
+from telelegal_app.models import User, Profile, Forum, Requests
 from telelegal_app.main.forms import ProfileForm, ForumForm, RequestForm
 from telelegal_app import bcrypt
-from functools import wraps
 
 # Import app and db from events_app package so that we can run app
 from telelegal_app import app, db
@@ -24,39 +23,41 @@ def homepage():
 
 @main.route('/create_profile', methods=['GET', 'POST'])
 @login_required
-def create_book():
-    form = BookForm()
+def create_profile():
+    form = ProfileForm()
 
     # if form was submitted and contained no errors
     if form.validate_on_submit(): 
-        new_book = Book(
-            title=form.title.data,
-            publish_date=form.publish_date.data,
-            author=form.author.data,
-            audience=form.audience.data,
-            genres=form.genres.data
+        new_profile = Profile(
+            full_name=form.full_name.data,
+            law_firm=form.law_firm.data,
+            state_bar_num=form.state_bar_num.data,
+            practice_areas=form.practice_areas.data,
+            years_exp=form.years_exp.data
         )
-        db.session.add(new_book)
+        db.session.add(new_profile)
         db.session.commit()
 
-        flash('New book was created successfully.')
-        return redirect(url_for('main.book_detail', book_id=new_book.id))
+        flash('New profile was created successfully.')
+        return redirect(url_for('main.book_detail', book_id=new_profile.id))
     return render_template('create_book.html', form=form)
 
 
 @main.route('/create_forum_post', methods=['GET', 'POST'])
 @login_required
-def create_author():
-    form = AuthorForm()
+def create_forum_post():
+    form = ForumForm()
     if form.validate_on_submit():
-        new_author = Author(
-            name=form.name.data,
-            biography=form.biography.data
+        new_post = Forum(
+            title=form.title.data,
+            post=form.post.data,
+            publish_date=form.publish_date.data,
+            author=form.author.data
         )
-        db.session.add(new_author)
+        db.session.add(new_post)
         db.session.commit()
 
-        flash('New author created successfully.')
+        flash('New post created successfully.')
         return redirect(url_for('main.homepage'))
     
     # if form was not valid, or was not submitted yet
@@ -64,17 +65,17 @@ def create_author():
 
 
 @main.route('/create_request', methods=['GET', 'POST'])
-@login_required
-def create_genre():
-    form = GenreForm()
+def create_request():
+    form = RequestForm()
     if form.validate_on_submit():
-        new_genre = Genre(
-            name=form.name.data
+        new_request = Requests(
+            full_name=form.full_name.data,
+
         )
-        db.session.add(new_genre)
+        db.session.add(new_request)
         db.session.commit()
 
-        flash('New genre created successfully.')
+        flash('New request submitted successfully.')
         return redirect(url_for('main.homepage'))
     
     # if form was not valid, or was not submitted yet
